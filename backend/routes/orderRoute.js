fa433fb423d1835643c1bf3fc60e5ec4f13baad7
Chasith-Randima <chasith19@gmail.com>
2023-12-26 23:47:01 +0530

    routes created

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const orderController = require("./../controllers/orderController");
+const authController = require("./../controllers/authController");
+const router = express.Router();
+
+router
+  .route("/")
+  .get(orderController.getAllOrders)
+  .post(authController.protect, orderController.createOneOrder);
+router
+  .route("/:id")
+  .get(orderController.getOneOrder)
+  .patch(authController.protect, orderController.updateAOrder)
+  .delete(authController.protect, orderController.deleteAOrder);
+
+module.exports = router;
